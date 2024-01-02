07fe4088a25cd70bf239b7af729342c7a5a49028
PeNoNyZeUs <126082032+PeNoNyZeUs@users.noreply.github.com>
2024-01-02 17:34:21 +0700

    Add details product

--- frontend/src/screens/detail/DetailPowerSupply.js
+++ frontend/src/screens/detail/DetailPowerSupply.js
@@ -11,16 +11,10 @@ function DetailPowerSupply() {
         <Environment preset="sunset" />
     </Canvas>
     <div className="container">
-    <h1>Earth</h1>
-    <p>"Planet Earth" redirects here. For other uses, see Earth (disambiguation) and Planet Earth (disambiguation).</p>
+    <h1>PowerSupply</h1>
+    <p>แหล่งจ่ายไฟ (Power Supply)</p>
     <p >
-    Earth is the third planet from the Sun and the only astronomical object known to harbor life. This is enabled by Earth being a water world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth's water is contained in its global ocean, covering 70.8% of Earth's crust. The remaining 29.2% of Earth's crust is land, most of which is located in the form of continental landmasses within one hemisphere, Earth's land hemisphere. Most of Earth's land is somewhat humid and covered by vegetation, while large sheets of ice at Earth's polar deserts retain more water than Earth's groundwater, lakes, rivers and atmospheric water combined. Earth's crust consists of slowly moving tectonic plates, which interact to produce mountain ranges, volcanoes, and earthquakes. Earth has a liquid outer core that generates a magnetosphere capable of deflecting most of the destructive solar winds and cosmic radiation.
-    </p>
-    <p>
-    Earth has a dynamic atmosphere, which sustains Earth's surface conditions and protects it from most meteoroids and UV-light at entry. It has a composition of primarily nitrogen and oxygen. Water vapor is widely present in the atmosphere, forming clouds that cover most of the planet. The water vapor acts as a greenhouse gas and, together with other greenhouse gases in the atmosphere, particularly carbon dioxide (CO2), creates the conditions for both liquid surface water and water vapor to persist via the capturing of energy from the Sun's light. This process maintains the current average surface temperature of 14.76 °C, at which water is liquid under atmospheric pressure. Differences in the amount of captured energy between geographic regions (as with the equatorial region receiving more sunlight than the polar regions) drive atmospheric and ocean currents, producing a global climate system with different climate regions, and a range of weather phenomena such as precipitation, allowing components such as nitrogen to cycle.
-    </p>
-    <p>
-    Earth is rounded into an ellipsoid with a circumference of about 40,000 km. It is the densest planet in the Solar System. Of the four rocky planets, it is the largest and most massive. Earth is about eight light-minutes away from the Sun and orbits it, taking a year (about 365.25 days) to complete one revolution. Earth rotates around its own axis in slightly less than a day (in about 23 hours and 56 minutes). Earth's axis of rotation is tilted with respect to the perpendicular to its orbital plane around the Sun, producing seasons. Earth is orbited by one permanent natural satellite, the Moon, which orbits Earth at 384,400 km (1.28 light seconds) and is roughly a quarter as wide as Earth. Through tidal locking, the Moon always faces Earth with the same side, which causes tides, stabilizes Earth's axis, and gradually slows its rotation.
+    แหล่งจ่ายไฟหรือที่มักจะเรียกทับศัพท์ว่าเพาว์เวอร์ซัพพลาย เป็นส่วนประกอบที่สำคัญส่วนหนึ่งทำหน้าที่แปลงสัญญาณ ไฟฟ้ากระแสสลับจากแหล่งกำเนิดให้เป็นไฟฟ้ากระแสตรงด้วยความต่างศักย์ที่เหมาะสมก่อนเข้าสู่คอมพิวเตอร์ โดยมีสายเชื่อมต่อไปยังอุปกรณ์ประกอบต่าง ๆ ภายในเครื่อง ซึ่งในการแปลงสัญญาณไฟฟ้าดังกล่าวนี้จะก่อให้เกิดความร้อนขึ้นด้วย ดังนั้นภายในแหล่งจ่ายไฟจึงต้องมีพัดลมเพื่อช่วยในการระบายความร้อนออกจากตัวเครื่องซึ่งมีความสำคัญมาก เพราะการที่เครื่องมีความร้อนที่สูงมาก ๆ นั้น อาจจะเกิดความเสียหายต่ออุปกรณ์ประกอบภายในเครื่องได้ง่าย ปกติแล้วมักจะไม่ค่อยมีการเลือกซื้อพาว์เวอร์ซัพพลายกันนักถ้าไม่ใช่เนื่องจากตัวเก่าที่ใช้อยู่เกิดเสียไปโดยมากเราจะเลือกซื้อ มาพร้อมกับเคส
     </p>
     </div>
 
--- frontend/src/screens/detail/DetailRam.js
+++ frontend/src/screens/detail/DetailRam.js
@@ -11,17 +11,16 @@ function DetailRam() {
         <Environment preset="sunset" />
     </Canvas>
     <div className="container">
-    <h1>Earth</h1>
-    <p>"Planet Earth" redirects here. For other uses, see Earth (disambiguation) and Planet Earth (disambiguation).</p>
+    <h1>Ram</h1>
+    <p>Ram (Random-access memory) หรือ แรม เป็นหน่วยความจำหลักของคอมพิวเตอร์ ซึ่งเป็นองค์ประกอบสำคัญที่มีผลต่อประสิทธิภาพการทำงานโดยรวม ทั้งยังส่งผลต่อความเร็วในการทำงานของระบบคอมพิวเตอร์ ไม่ว่าอุปกรณ์ชนิดนั้นจะเป็นสมาร์ทโฟน, แท็บเล็ต, คอมพิวเตอร์ หรืออุปกรณ์ใดๆ ที่จำเป็นต้องอ่านและเขียนคำสั่งไปยังหน่วยความจำ</p>
     <p >
-    Earth is the third planet from the Sun and the only astronomical object known to harbor life. This is enabled by Earth being a water world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth's water is contained in its global ocean, covering 70.8% of Earth's crust. The remaining 29.2% of Earth's crust is land, most of which is located in the form of continental landmasses within one hemisphere, Earth's land hemisphere. Most of Earth's land is somewhat humid and covered by vegetation, while large sheets of ice at Earth's polar deserts retain more water than Earth's groundwater, lakes, rivers and atmospheric water combined. Earth's crust consists of slowly moving tectonic plates, which interact to produce mountain ranges, volcanoes, and earthquakes. Earth has a liquid outer core that generates a magnetosphere capable of deflecting most of the destructive solar winds and cosmic radiation.
-    </p>
+    RAM จัดว่าเป็นหน่วยความจำชั่วคราว (Short-Term Memory) โดยการทำงานของมันนั้นจะเป็นการเขียนหรือบันทึกข้อมูลแบบสุ่ม ซึ่งหน่วยความจำชนิดนี้จะสามารถบันทึกข้อมูลลงในตำแหน่งต่างๆ ได้อย่างอิสระ ทั้งนี้ก็เพื่อเพิ่มความเร็วในการบันทึกและอ่านข้อมูล จึงเป็นที่มาของคำว่า Random access ขณะเดียวกันก็ทำให้มันแตกต่างจากหน่วยความจำประเภทอื่นๆ อย่างสิ้นเชิง เมื่อเทียบกับฮาร์ดไดร์ฟ (Hard Drive) หรือ SSD (ที่รู้จักกันในนามของ Direct Access Memory) สิ่งที่ทำให้มันแตกต่างจากหน่วยความจำที่เข้าถึงได้โดยตรงนั่นก็คือ เมื่อมีการตัดกระแสไฟฟ้าหรือปิดคอมพิวเตอร์ ข้อมูลที่อยู่ภายใน Ram ก็จะหายไปอย่างสมบูรณ์    </p>
     <p>
-    Earth has a dynamic atmosphere, which sustains Earth's surface conditions and protects it from most meteoroids and UV-light at entry. It has a composition of primarily nitrogen and oxygen. Water vapor is widely present in the atmosphere, forming clouds that cover most of the planet. The water vapor acts as a greenhouse gas and, together with other greenhouse gases in the atmosphere, particularly carbon dioxide (CO2), creates the conditions for both liquid surface water and water vapor to persist via the capturing of energy from the Sun's light. This process maintains the current average surface temperature of 14.76 °C, at which water is liquid under atmospheric pressure. Differences in the amount of captured energy between geographic regions (as with the equatorial region receiving more sunlight than the polar regions) drive atmospheric and ocean currents, producing a global climate system with different climate regions, and a range of weather phenomena such as precipitation, allowing components such as nitrogen to cycle.
-    </p>
+    เนื่องจากลักษณะเฉพาะของมัน ข้อมูลที่ถูกจัดการโดย RAM จึงมีการเปลี่ยนแปลงหรือผันผวนอยู่ตลอดเวลา และมีการประมวลผลที่เกิดขึ้นเฉพาะในช่วงเวลาที่กำหนดเท่านั้น    </p>
     <p>
-    Earth is rounded into an ellipsoid with a circumference of about 40,000 km. It is the densest planet in the Solar System. Of the four rocky planets, it is the largest and most massive. Earth is about eight light-minutes away from the Sun and orbits it, taking a year (about 365.25 days) to complete one revolution. Earth rotates around its own axis in slightly less than a day (in about 23 hours and 56 minutes). Earth's axis of rotation is tilted with respect to the perpendicular to its orbital plane around the Sun, producing seasons. Earth is orbited by one permanent natural satellite, the Moon, which orbits Earth at 384,400 km (1.28 light seconds) and is roughly a quarter as wide as Earth. Through tidal locking, the Moon always faces Earth with the same side, which causes tides, stabilizes Earth's axis, and gradually slows its rotation.
-    </p>
+    ปัจจุบัน โมดูล Ram รูปแบบใหม่กำลังได้รับการพัฒนาอย่างต่อเนื่อง เพื่อให้สามารถจัดเก็บงาน (Task) อื่นๆ ซึ่งหมายถึงการดำเนินการบางอย่าง เช่น การเริ่มต้นระบบ (System Start Up) ที่อาจเกิดได้เร็วขึ้นกว่าเดิมเนื่องจากการกระทำเหล่านั้นได้มีการเตรียมพร้อมสำหรับเรียกใช้    </p>    
+    <p>
+    RAM มีหน้าที่ในการเก็บชุดคำสั่งและข้อมูลในขณะที่ระบบคอมพิวเตอร์กำลังทำงานอยู่ ทั้งในแบบของ Input และ Output โดยการเข้าถึงข้อมูลของ RAM นั้นจะเป็นการเข้าถึงแบบสุ่ม (Random Access) ซึ่งหมายถึง CPU สามารถเข้าถึงทุกๆ ส่วนของหน่วยความจำหรือพื้นที่เก็บข้อมูลได้โดยตรง เพื่อเพิ่มความเร็วในกระบวนการเขียนและอ่านข้อมูลอย่างต่อเนื่อง</p>   
     </div>
 
   </>
